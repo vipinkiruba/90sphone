476dae9709b4086a158da302d7955b2a0694e989
Vipin Kiruba <67626515+vipinkiruba@users.noreply.github.com>
2021-07-01 15:13:26 +0530

    version1

---
+++ index.js
@@ -0,0 +1,63 @@
+
+
+document.getElementById("1").addEventListener("click", function () {
+    var audio = new Audio('sounds/apidipodu.mp3');
+    audio.play();
+});
+
+document.getElementById("2").addEventListener("click", function () {
+    var audio = new Audio('sounds/bark.mp3');
+    audio.play();
+});
+
+document.getElementById("3").addEventListener("click", function () {
+    var audio = new Audio('sounds/devuda.mp3');
+    audio.play();
+});
+
+document.getElementById("4").addEventListener("click", function () {
+    var audio = new Audio('sounds/ilu.mp3');
+    audio.play();
+});
+
+document.getElementById("5").addEventListener("click", function () {
+    var audio = new Audio('sounds/kannum.mp3');
+    audio.play();
+});
+
+document.getElementById("6").addEventListener("click", function () {
+    var audio = new Audio('sounds/littlebu.mp3');
+    audio.play();
+});
+
+
+
+document.getElementById("7").addEventListener("click", function () {
+    var audio = new Audio('sounds/apidipodu.mp3');
+    audio.play();
+});
+
+document.getElementById("8").addEventListener("click", function () {
+    var audio = new Audio('sounds/bark.mp3');
+    audio.play();
+});
+
+document.getElementById("9").addEventListener("click", function () {
+    var audio = new Audio('sounds/devuda.mp3');
+    audio.play();
+});
+
+document.getElementById("10").addEventListener("click", function () {
+    var audio = new Audio('sounds/ilu.mp3');
+    audio.play();
+});
+
+document.getElementById("11").addEventListener("click", function () {
+    var audio = new Audio('sounds/kannum.mp3');
+    audio.play();
+});
+
+document.getElementById("12").addEventListener("click", function () {
+    var audio = new Audio('sounds/littlebu.mp3');
+    audio.play();
+});
